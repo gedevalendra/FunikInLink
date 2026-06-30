3533be12c5ef62d0cdfff3157e6265ca5e2aea75
Avardhra <gedevalendra738@gmail.com>
2026-06-30 09:37:13 +0700

    update-otomatis

--- lib/models.ts
+++ lib/models.ts
@@ -5,6 +5,19 @@ const LinkSchema = new mongoose.Schema({
   title: { type: String, required: true },
   description: { type: String, default: "" },
   url: { type: String, required: true },
+}, { 
+  collection: "SharedLink" 
 });
 
 export const SharedLink = mongoose.models.SharedLink || mongoose.model("SharedLink", LinkSchema);
+
+
+const UserSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+}, { 
+  collection: "User" 
+});
+
+export const User = mongoose.models.User || mongoose.model("User", UserSchema);
