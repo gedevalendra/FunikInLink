6fa732f6bc8a7471c3d1268f416eccf7cb1d7a26
Avardhra <gedevalendra738@gmail.com>
2026-06-30 12:28:31 +0700

    update-otomatis

--- lib/models.ts
+++ lib/models.ts
@@ -5,7 +5,7 @@ const LinkSchema = new mongoose.Schema({
   username: { type: String, required: true }, 
   icon: { type: String, default: "bx-link" },
   title: { type: String, required: true },
-  description: { type: String, default: "" },
+  description: { type: String, default: "\"\"" },
   url: { type: String, required: true },
 }, { 
   collection: "SharedLink" 
@@ -20,20 +20,21 @@ const AdminSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   bio: { type: String, default: "" },
   hashtags: { type: [String], default: [] },
-  isNewUser: { type: Boolean, default: true }, // <-- TAMBAHKAN PROPERTI INI
+  isNewUser: { type: Boolean, default: true }, 
 }, { 
   collection: "Admin" 
 });
 
 export const Admin = mongoose.models.Admin || mongoose.model("Admin", AdminSchema);
 
-// 3. Model untuk Hak Akses Admin Panel
+// 3. Model untuk Hak Akses Admin Panel (PASTIKAN BAGIAN INI ADA)
 const AdminListSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   nama: { type: String, required: true },
   iduser: { type: String },
 }, { 
-  collection: "AdminList" 
+  collection: "AdminList", // Pastikan nama collection sesuai di MongoDB Compass Anda
+  timestamps: true 
 });
 
 export const AdminList = mongoose.models.AdminList || mongoose.model("AdminList", AdminListSchema);
